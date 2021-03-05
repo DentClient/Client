3fc4b3c1e1f832fde4f30c9edbbe94994dd790a1
invertedcube <58447430+invertedcube@users.noreply.github.com>
2021-03-05 14:11:31 -0800

    Made nofall work sorta

--- src/main/java/net/dent/client/DentClientMod.java
+++ src/main/java/net/dent/client/DentClientMod.java
@@ -6,14 +6,13 @@ public class DentClientMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
         System.out.println("########################################");
-        System.out.println("    INITIALIZING DENT CLIENT V 0.1.42     ");
+        System.out.println("    INITIALIZING DENT CLIENT V 0.42     ");
         System.out.println("########################################");
-        System.out.println(" ");
-        System.out.println(" ");
-        System.out.println(" ");
+        System.out.println();
+        System.out.println();
+        System.out.println();
         System.out.println("Dent Client Credits:   ");
-        System.out.println("    - Head Dev: Whop42/DentClientOfficial");
-        System.out.println("    - Junior Dev: InvertedCube");
-        System.out.println("    - ClickGUI/Parts of Base: Enaium");
+        System.out.println("    - Head Dev: Whop42");
+        System.out.println("    - Sucky Co-Dev: invertedcube");
 	}
 }
--- src/main/java/net/dent/client/module/modules/player/NoFall.java
+++ src/main/java/net/dent/client/module/modules/player/NoFall.java
@@ -5,18 +5,23 @@ import net.dent.client.event.events.EventUpdate;
 import net.dent.client.module.Category;
 import net.dent.client.module.Module;
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.network.packet.c2s.play.PlayerActionC2SPacket;
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 
 public class NoFall extends Module {
+    private boolean PlayerMoveC2SPacket;
+
     public NoFall() {
         super("No Fall", -1, Category.PLAYER, false);
 
     }
 
+
+
     @EventTarget
     public void onUpdate(EventUpdate e) {
         ClientPlayerEntity player = mc.player;
-        if(player.fallDistance <= (player.isFallFlying() ? 1 : 2))
+        if(player.fallDistance <= (player.isFallFlying() ? 1 : 2) && (player.fallDistance >= 2))
             return;
 
         if(player.isFallFlying() && player.isSneaking()
@@ -24,11 +29,21 @@ public class NoFall extends Module {
             return;
 
         player.networkHandler.sendPacket(new PlayerMoveC2SPacket(true));
+
+
+    if (PlayerMoveC2SPacket) {
+        player.networkHandler.sendPacket(new PlayerMoveC2SPacket(true));
+    }
+
+
+
+
     }
 
     private boolean isFallingFastEnoughToCauseDamage(ClientPlayerEntity player)
     {
         return player.getVelocity().y < -0.5;
+
     }
 
 }
